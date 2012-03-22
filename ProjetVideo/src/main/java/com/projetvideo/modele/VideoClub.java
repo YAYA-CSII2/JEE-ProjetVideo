36716357481873e9f983b0d0e36a0d586c6ca513
KURDIJAKA Yann <ykurdijaka@epsi.fr>
2012-03-22 16:23:08 +0100

    Singleton + test

--- ProjetVideo/src/main/java/com/projetvideo/modele/VideoClub.java
+++ ProjetVideo/src/main/java/com/projetvideo/modele/VideoClub.java
@@ -1,5 +1,13 @@
 package com.projetvideo.modele;
 
 public class VideoClub {
-    
+
+    private static VideoClub instance = null;
+
+    public static VideoClub getInstance() {
+        if (instance == null) {
+            instance = new VideoClub();
+        }
+        return instance;
+    }
 }
--- ProjetVideo/src/test/java/test/projetvideo/modele/TestVideoClub.java
+++ ProjetVideo/src/test/java/test/projetvideo/modele/TestVideoClub.java
@@ -2,6 +2,8 @@ package test.projetvideo.modele;
 
 import com.projetvideo.modele.VideoClub;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotSame;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -11,11 +13,25 @@ public class TestVideoClub {
             
     @Before
     public void setUp() {
-        vc = new VideoClub();
+        vc = VideoClub.getInstance();
     }
     
     @Test
     public void instanceExiste() {
         assertNotNull(vc);
+    }
+    
+    @Test
+    public void testSingletonFonctionne() {
+        VideoClub vc2 = VideoClub.getInstance();
+        
+        assertEquals(vc, vc2);
+    }
+    
+    @Test
+    public void testSingletonFonctionne2() {
+        VideoClub vc2 = new VideoClub();
+        
+        assertNotSame(vc, vc2);
     }
 }
